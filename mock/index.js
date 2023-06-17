f5013a927d0f7a95e70906a68a1dc3a517aa14e5
蔡徐坤打篮球 <88469720+Cxk-1233@users.noreply.github.com>
2023-06-17 20:35:34 +0800

    Delete mock directory

--- mock/index.js
+++
@@ -1,7 +0,0 @@
-import Mock from "mockjs"
-import {getRouteData} from "./data/indexData";
-import http from "@/utils/request";
-
-Mock.mock(`${http.getUri()}/route`, function () {
-    return getRouteData();
-});
